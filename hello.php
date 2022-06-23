6feefdaa8c6c7ded8605bb5e79d444926f36cdd8
Didix6 <didi-twix@live.fr>
2022-06-23 15:06:38 +0200

    challenge1 php

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+$bookTitle = "Voyage avec un âne dans les Cévennes";
+$readed = "False";
+$parutionYear = 2017;
+$bookPrice = 5.70;
+
+echo $bookTitle;
+if ($readed) {
+    echo "Pas lu";
+}
+echo $parutionDate;
+echo $price;
+?>
